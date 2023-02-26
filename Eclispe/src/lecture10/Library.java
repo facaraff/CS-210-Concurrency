7fb822be8cc95ebb447ff41c657454ff357d3b92
facaraff <fabio.caraffini@gmail.com>
2023-02-26 11:57:24 +0000

    added some comment

--- Eclispe/src/lecture10/Library.java
+++ Eclispe/src/lecture10/Library.java
@@ -1,19 +1,20 @@
 package lecture10;
 
 public class Library{
+	
     static final int MAX_AVAIALABLE = 2;
     private boolean[] booksInUse = new boolean[MAX_AVAIALABLE];
-    public synchronized void borrowBook(String name)
-            throws InterruptedException{
+    
+    public synchronized void borrowBook(String name) throws InterruptedException{
         System.out.println(name + " is waiting!");
-        while(areAllTrue()){
-            wait();
-        }
+        while(areAllTrue()){//are all the books being taken?
+            wait();//if so, then wait!
+        }//if not, take the book and release to lock! (notify the waiting threads)
         System.out.println(name + " is trying to get a book!");
         takeFirstAvaialbleBook();
         notifyAll();
     }
-    private boolean areAllTrue(){
+    private boolean areAllTrue(){// no need to synchronise this -> it's only called from a synchronised method!
         for(boolean b : booksInUse) if(!b) return false;
         return true;
     }
