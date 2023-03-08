aff79c73e8164a4ef066c436281ea5de65acb8c6
facaraff <fabio.caraffini@gmail.com>
2023-03-08 17:31:56 +0000

    lecture 15 java code added
    Signed-off-by: facaraff <fabio.caraffini@gmail.com>

---
+++ Eclispe/src/lecture15/SingleLaneBridge.java
@@ -0,0 +1,40 @@
+package lecture15;
+
+public class SingleLaneBridge {
+
+ 
+   public static void main(String[] args) throws InterruptedException {
+	   
+        BridgeQueue bridge = new BridgeQueue();
+        
+        WestCar westCarA = new WestCar("WA", bridge);
+        WestCar westCarB = new WestCar("WB", bridge);
+        
+        EastCar eastCarA = new EastCar("EA", bridge);
+        EastCar eastCarB = new EastCar("EB", bridge);
+        
+        Thread twa = new Thread(westCarA);
+        Thread twb = new Thread(westCarB);
+        Thread tea = new Thread(eastCarA);
+        Thread teb = new Thread(eastCarB);
+        
+        twa.start();
+        twb.start();
+        tea.start();
+        teb.start();
+        
+        Thread.sleep(10000);// sleep for a while
+        
+        twa.interrupt();
+        twb.interrupt();
+        tea.interrupt();
+        teb.interrupt();
+        
+        twa.join();
+        twb.join();
+        tea.join();
+        teb.join();
+        
+        System.out.println("Program has ended.");
+    }   
+}
